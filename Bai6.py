c4290adea884776ca73c538d847f8e278e5ae05f
BaoLong <baolongkg2020@gmail.com>
2026-04-17 11:42:45 +0700

    add baitap

---
+++ Bai6.py
@@ -0,0 +1,13 @@
+n = int(input("Nhập số nguyên dương n: "))
+
+max_digit = 0
+
+while n > 0:
+    digit = n % 10
+    
+    if digit > max_digit:
+        max_digit = digit
+    
+    n //= 10
+
+print("Số lớn nhất =", max_digit)
