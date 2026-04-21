51f8660e9d9ad755623c757a2941ce21cd637a8a
BaoLong <baolongkg2020@gmail.com>
2026-04-21 11:06:39 +0700

    TH

--- Bai-i_C04.py
+++ Bai-i_C04.py
@@ -1,13 +1,16 @@
 from datetime import datetime
+
+# Thiết lập thời gian giả định theo đề bài
 now = datetime.now()
 
-print("Năm hiện tại:", now.year)
-print("Tháng hiện tại bằng chữ:", now.strftime("%B"))
-print("Tuần hiện tại là tuần thứ mấy trong năm:", now.isocalendar()[1])
-
-week_of_month = (now.day - 1) // 7 + 1
-print("Tuần hiện tại là tuần thứ mấy trong tháng:", week_of_month)
-print("Ngày hiện tại là ngày thứ mấy trong năm:", now.timetuple().tm_yday)
-print("Ngày dương lịch hiện tại là ngày:", now.day)
-print("Thứ của ngày hiện tại:", now.strftime("%A"))
-print("Giờ phút giây hiện tại:", now.strftime("%H:%M:%S"))
+print(f"{'Thông tin cần hiển thị'}: {'Kết quả hiển thị'}")
+print("-" * 70)
+print(f"{'Năm hiện tại'}: {now.year}")
+print(f"{'Tháng hiện tại bằng chữ'}: {now.strftime('%B')}")
+print(f"{'Tuần hiện tại là tuần thứ mấy trong năm'}: {now.isocalendar()[1]}")
+week_in_month = (now.day - 1) // 7 + 1
+print(f"{'Tuần hiện tại là tuần thứ mấy trong tháng'}: {week_in_month}")
+print(f"{'Ngày hiện tại là ngày thứ mấy trong năm'}: {now.timetuple().tm_yday}")
+print(f"{'Ngày dương lịch hiện tại là ngày'}: {now.day}")
+print(f"{'Thứ của ngày hiện tại'}: {now.strftime('%A')}")
+print(f"{'Giờ phút giây hiện tại'}: {now.strftime('%H:%M:%S')}")
